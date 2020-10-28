1361593e3ee2cfae6b218839b1a2b3b30002676d
Dima <nikitin_dima200dn@mail.ru>
2020-10-28 17:29:28 +0300

    class coor and class Greeting

--- First.cpp
+++ First.cpp
@@ -2,6 +2,35 @@
 #include <string>
 using namespace std;
 
+class coor
+{
+	private:
+	int x;
+	int y;
+
+	public:
+	coor(int x, int y)
+	{
+		this->x=x;
+		this->y=y;
+	{
+	
+	string print()
+	{
+		return "X = " + x + " Y = "+y;
+	}
+
+};
+
+class Print :public coor
+{
+	public:
+	void display()
+	{
+	cout<<print()<<endl;
+	}
+};
+
 
 string foo(string name)
 {
@@ -9,9 +38,17 @@ return "Hello, "+name;
 }
 int main()
 {
+cout<<"How are you?"<<endl;
 string x;
 cin>>x;
 cout<<foo(x)<<endl;
+cout<<"Enter two numbers"<<endl;
+int x1,y1;
+cin>>x1>>y1;
+coor v1(x1,y1);
+Print m1;
+m1.display();
+
 return 0;
 }
 
